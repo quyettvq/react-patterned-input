073a1898ae24c8f31fdf3e6d8692accaa146cd85
Tran Van Quyet <quyettvq@gmail.com>
2020-09-08 16:32:00 +0700

    update demo

--- demo/frontend/src/App.js
+++ demo/frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PatternedInput from '../../../index';
 import dateTemplate from './dateTemplate';
+import {translate} from 'i18n-zone';
 
 export default function App({}) {
     return <div>
+        <h3>{translate('Enter date :format', {format: 'dd/mm/yyyy'})}</h3>
         <PatternedInput
             template={dateTemplate}
             onChange={({year, month, date}) => console.log(year, month, date)}
--- demo/frontend/src/index.js
+++ demo/frontend/src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import i18n from 'i18n-zone';
 
-ReactDOM.render(<App/>, document.getElementById('app-root'));
-
 i18n.settings.setResource('en', {});
-console.log(i18n.translate('Hahahahaa'));
+i18n.settings.setResource('vi', {
+    'Enter date :format': 'Nhập ngày :format',
+});
+i18n.settings.setLocale('vi');
 
+ReactDOM.render(<App/>, document.getElementById('app-root'));
